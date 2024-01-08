96c48cf104540e8f3956ec38d8cb38edde9a8018
dmytro.voloshyn <dima.voloshin@hotmail.com>
2024-01-08 17:54:59 +0200

    Create logger

---
+++ Homework/Homework5/Actions.cs
@@ -0,0 +1,25 @@
+﻿namespace Homework5
+{
+    public class Actions
+    {
+        private readonly Logger logger = Logger.Instance;
+
+        public Result FirstMethod()
+        {
+            logger.Log(LogLevel.Info, "Start method: FirstMethod");
+            return new Result();
+        }
+
+        public Result SecondMethod()
+        {
+            logger.Log(LogLevel.Warning, "Skipped logic in method: SecondMethod");
+            return new Result();
+        }
+
+        public Result ThirdMethod()
+        {
+            logger.Log(LogLevel.Error, "Action failed by a reason: I broke a logic");
+            return new Result { Status = false };
+        }
+    }
+}
---
+++ Homework/Homework5/Services/LoggerService.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using Homework5;
+
+class Logger
+{
+    private static Logger? instance;
+    private string received_log = "";
+
+    private Logger() { }
+
+    public static Logger Instance
+    {
+        get
+        {
+            instance ??= new Logger();
+            return instance;
+        }
+    }
+
+    private static string FormatLog(string logTime, LogLevel logLevel, string logMessage)
+    {
+        StringBuilder formattedLog = new();
+
+        formattedLog.Append($"{logTime}: ");
+        formattedLog.Append($"{logLevel}: ");
+        formattedLog.Append(logMessage);
+
+        return formattedLog.ToString();
+    }
+
+    private static void ChangeLogColor(LogLevel loglevel)
+    {
+        _ = loglevel switch
+        {
+            LogLevel.Info => Console.ForegroundColor = ConsoleColor.Green,
+            LogLevel.Warning => Console.ForegroundColor = ConsoleColor.DarkYellow,
+            LogLevel.Error => Console.ForegroundColor = ConsoleColor.DarkRed,
+            _ => throw new NotImplementedException()
+        };
+    }
+
+
+    public void Log(LogLevel logLevel, string logMessage)
+    {
+        ChangeLogColor(logLevel);
+
+        string logTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+        string formattedLog = FormatLog(logTime, logLevel, logMessage);
+
+        Console.WriteLine(formattedLog);
+        received_log += formattedLog + "\n";
+    }
+
+    public void WriteLogsToFile()
+    {
+        File.WriteAllText("log.txt", received_log);
+    }
+}
