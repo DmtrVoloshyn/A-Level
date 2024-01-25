0fe9995f8b5049b9753499af10fd69d5610ac4d0
dmytro.voloshyn <dima.voloshin@hotmail.com>
2024-01-25 20:05:29 +0200

    added app file

---
+++ Homework/Logger/App.cs
@@ -0,0 +1,32 @@
+﻿namespace Logger
+{
+    public class App
+    {
+        private readonly Actions _actions;
+
+        public App(Actions actions)
+        {
+            _actions = actions;
+        }
+        public void Run()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var random = new Random().Next(1, 4);
+
+                switch (random)
+                {
+                    case 1:
+                        _actions.FirstMethod();
+                        break;
+                    case 2:
+                        _actions.SecondMethod();
+                        break;
+                    default:
+                        _actions.ThirdMethod();
+                        break;
+                }
+            }
+        }
+    }
+}
