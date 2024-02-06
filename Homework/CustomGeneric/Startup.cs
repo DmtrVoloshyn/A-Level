47485ad88c590b2349a273c3e8e53a0e2e9c4fc5
Dmytro <154446997+DmtrVoloshyn@users.noreply.github.com>
2024-02-06 08:14:54 +0000

    fix formating

--- Homework/CustomGeneric/Startup.cs
+++ Homework/CustomGeneric/Startup.cs
@@ -11,8 +11,8 @@
 			CustomCollection<int> customCollection = new CustomCollection<int>();
 
 			customCollection.Add(5);
-            customCollection.Add(10);
-            customCollection.Add(1003);
+			customCollection.Add(10);
+			customCollection.Add(1003);
 
 			Console.WriteLine("Default collection: ");
 			foreach (var item in customCollection)
@@ -20,20 +20,20 @@
 				Console.WriteLine(item);
 			}
 
-            Console.WriteLine("\nSorted collectin: ");
-            customCollection.Sort();
-            foreach (var item in customCollection)
-            {
-                Console.WriteLine(item);
-            }
+			Console.WriteLine("\nSorted collectin: ");
+			customCollection.Sort();
+			foreach (var item in customCollection)
+			{
+				Console.WriteLine(item);
+			}
 
-            Console.WriteLine("\nCollection with default value set to 0: ");
+			Console.WriteLine("\nCollection with default value set to 0: ");
 			customCollection.SetDefaultAt(index: 0);
 			foreach (var item in customCollection)
 			{
 				Console.WriteLine(item);
 			}
-        }
-    }
+		}
+	}
 }
 
